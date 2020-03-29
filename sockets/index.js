4e61ae9dbf0ebdcc0e3448a927737a1ea84b8a95
peter smith <xorai@hotmail.com>
2020-03-29 18:39:46 -0500

    added call user button

--- sockets/index.js
+++ sockets/index.js
@@ -1,9 +1,32 @@
 module.exports = io => {
   io.on('connection', function(socket) {
     console.log('new client connected')
+    console.log(Object.keys(io.sockets.sockets))
+    // console.log(io.sockets.clients()[1])
     socket.on('message', function(data) {
       io.emit('message', { data })
       console.log(data)
     })
+    
+    socket.on("call-user", data => {
+      console.log(Object.keys(io.sockets.sockets)[1], " Calling user.");
+      // console.log(data.offer, " Data offer");
+      console.log("FROM: ", socket.id, "Socket ID");
+      socket.to(Object.keys(io.sockets.sockets)[1]).emit("call-made", {
+        offer: data.offer,
+        socket: socket.id
+      });
+    });
+
+    socket.on("make-answer", data => {
+      console.log(data.to, " Data to.");
+      // console.log(data.answer, "Data answer");
+      console.log("FROM: ", socket.id, "Socket ID");
+      socket.to(data.to).emit("answer-made", {
+        socket: socket.id,
+        answer: data.answer
+      });
+    });
+
   })
 }
